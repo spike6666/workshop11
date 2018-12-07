bc8f8bc348a9a7f5f56852ddbf7ea6fb588ae0dd
spike6666 <44088157+spike6666@users.noreply.github.com>
2018-12-07 15:09:40 +0000

    php file

---
+++ variable.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Task Week 11</title>
+</head>
+<body>
+<?php
+$name = "Spike";
+$school = "University of central Lancashire";
+$city = "Guangzhou";
+$thing = "noval";
+$book = 1984 ;
+echo "Hello, my name is $name and I study $school.<br>
+I come from $city and my favourite $thing is $book. ";?>
+</body>
+</html>
